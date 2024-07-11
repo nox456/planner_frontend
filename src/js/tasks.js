757646c301b757e160ae7e439c79c1558da6b424
Gabriel_Vieira <nox030705@gmail.com>
2024-07-11 16:52:41 -0400

    feat(tasks): show modal asking if delete a task

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -19,12 +19,19 @@ if (auth.status == 401) {
     const { tasks } = data;
 
     if (tasks.length == 0) {
-        tasks_container.innerText = "No hay tareas, por ahora..."
+        tasks_container.innerText = "No hay tareas, por ahora...";
     } else {
         tasks.forEach((task) => {
-            const container = document.createElement("article")
+            const container = document.createElement("article");
             container.innerHTML = `
                 <h3>${task.name}</h3>
+                <dialog>
+                    <h3>Eliminar Tarea</h3>
+                    <p>${task.name}</p>
+                    <p>¿Estás seguro?</p>
+                    <button id="close-button">No</button>
+                    <button id="confirm-button">Si</button>
+                </dialog>
                 <button id="delete-button">Eliminar</button>
                 <hr>
                 <p>${task.course}</p>
@@ -32,19 +39,27 @@ if (auth.status == 401) {
                 <p>${task.evaluation}</p>
                 <hr>
                 <p>${task.finish_date}</p>
-            `
-            tasks_container.appendChild(container)
-            const delete_button = container.querySelector("#delete-button")
-            delete_button.addEventListener("click", async (e) => {
-                e.preventDefault()
+            `;
+            tasks_container.appendChild(container);
+            const delete_button = container.querySelector("#delete-button");
+            delete_button.addEventListener("click", async () => {
+                container.querySelector("dialog").showModal();
+            });
+            const close_button = container.querySelector("#close-button")
+            close_button.addEventListener("click", () => {
+                container.querySelector("dialog").close()
+            })
+            const confirm_button = container.querySelector("#confirm-button")
+            confirm_button.addEventListener("click", async () => {
+                await fetch(`http://localhost:4000/tasks?id=${task.id}`, {
+                    method: "DELETE"
+                })
+                container.querySelector("dialog").close()
                 container.remove()
                 if (tasks_container.children.length == 0) {
                     tasks_container.innerText = "No hay tareas, por ahora..."
                 }
-                await fetch(`http://localhost:4000/tasks?id=${task.id}`, {
-                    method: "DELETE"
-                })
             })
-        })
+        });
     }
 }
