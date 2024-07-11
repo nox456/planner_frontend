e9e8bd7cb8b679225527dd4e91211b16f6880437
Gabriel_Vieira <nox030705@gmail.com>
2024-07-11 10:55:36 -0400

    feat(signup): signup user and redirect to 'tareas'

---
+++ src/js/signup.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#form");
+const username_input = form.querySelector("#username");
+const password_input = form.querySelector("#password");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = username_input.value;
+    const password = password_input.value;
+    const res = await fetch("http://localhost:4000/auth/signup", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+    if (res.status == 401) {
+        document.querySelector("#message").innerText = "Este usuario ya existe!"
+    } else {
+        location.href = 'tareas.html'
+    }
+});
+
